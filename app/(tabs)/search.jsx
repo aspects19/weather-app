0ffced7ac258272500307af5e053d9482a6edb07
aspect <jeffamenya388@gmail.com>
2024-09-14 14:02:05 +0300

    Added a searchbar

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -1,13 +1,22 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import Home from './home'
+import { View, Text, ScrollView, TextInput } from 'react-native'
+import React from 'react';
+import Home from './home';
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const search = () => {
   return (
-    <ScrollView className='h-full bg-primary'>
-      <Home/>
-    </ScrollView>
-    
+    <SafeAreaView>
+      <ScrollView className='h-full bg-primary'>
+        <TextInput
+          placeholder='Type a location'
+          placeholderTextColor={"#333941"}
+          className='absolute h-8  bg-[#c1c3c5] z-10 top-[60px] left-16 right-16 rounded-lg pl-3 '
+        />
+          
+        <Home/>
+        
+      </ScrollView>
+    </SafeAreaView>
     
   )
 }
