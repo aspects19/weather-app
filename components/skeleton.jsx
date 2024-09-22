a6395c028bfc071bdb023dbe4d3ea38db26ed2d2
aspect <jeffamenya388@gmail.com>
2024-09-22 23:58:02 +0300

    Completed the skeleton component when the app is Loading

--- components/skeleton.jsx
+++ components/skeleton.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import { MotiView } from 'moti';
-import SkeletonExpo from 'moti/build/skeleton/expo';
+import { MotiView, ScrollView } from 'moti';
 import { Skeleton } from 'moti/skeleton';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SkeletonLoader = ({ width, height, borderRadius }) => {
+const SkeletonLoader = ({ width, height, borderRadius, padding }) => {
   return (
     <Skeleton 
       colorMode='dark' 
@@ -19,12 +18,13 @@ const SkeletonLoader = ({ width, height, borderRadius }) => {
 };
 
 const HomeSkeleton = () => {
-  const Spacer = ({ height = 16 }) => <View style={{ height }} />;
+  const Spacer = ({ height = 16, width = 16 }) => <View style={{ height, width }} />;
   return (
     <SafeAreaView className='h-full '>
       <MotiView 
         transition={{
-          type: 'timing'
+          type: 'timing',
+          repeatReverse: false,
         }}
         style={{
           height: '100%'
@@ -35,20 +35,32 @@ const HomeSkeleton = () => {
             <SkeletonLoader width={240} height={40} borderRadius={8} />
             <SkeletonLoader width={40} height={40} borderRadius={8} />
           </View>
-          <Spacer height={8}/>
-          <SkeletonLoader width={150} height={150} borderRadius={75} />
-          <SkeletonLoader width="60%" height={30} borderRadius={8} />
-          <SkeletonLoader width="40%" height={20} borderRadius={8} />
-          <View className='flex flex-row'>
-            <SkeletonLoader width={100} height={50} borderRadius={8} />
-            <SkeletonLoader width={100} height={50} borderRadius={8} />
-            <SkeletonLoader width={100} height={50} borderRadius={8} />
+          <Spacer height={36}/>
+          <SkeletonLoader width={140} height={30} borderRadius={8} />
+          <Spacer height={30}/>
+          <SkeletonLoader width={170} height={150} borderRadius={30} />
+          <Spacer height={40}/>
+          <SkeletonLoader width={110} height={40} borderRadius={4} />
+          <Spacer height={34}/>
+          <View className='flex flex-row w-11/12 justify-between px-9 mb-4'>
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
           </View>
-          <View className='flex flex-row'>
-            <SkeletonLoader width={80} height={100} borderRadius={8} />
-            <SkeletonLoader width={80} height={100} borderRadius={8} />
-            <SkeletonLoader width={80} height={100} borderRadius={8} />
+          <View className='flex flex-row w-11/12 justify-between px-9 mb-4'>
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
+            <SkeletonLoader width={60} height={35} borderRadius={8} />
           </View>
+          <Spacer height={45} />
+          <ScrollView horizontal={true} className='flex flex-row w-full '>
+            <Spacer width={15} />
+            <SkeletonLoader width={100} height={120} borderRadius={8} />
+            <Spacer width={30} />
+            <SkeletonLoader width={100} height={120} borderRadius={8} />
+            <Spacer width={30} />
+            <SkeletonLoader width={100} height={120} borderRadius={8} />
+          </ScrollView>
         </View>
       </MotiView>
     </SafeAreaView>
