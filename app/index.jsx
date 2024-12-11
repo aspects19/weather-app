fb03dc2329708911c428af4225d0ab66e4a71987
Jeff <jeffamenya388@gmail.com>
2024-12-11 21:08:53 +0300

    Remove unused code

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,6 @@ import { View } from 'react-native';
 import { Redirect } from 'expo-router';
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 
 const Welcome = () => {
   return (
