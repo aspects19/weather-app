550cc156aeaf3bfea00e896abcfe2e3caaa88cb8
aspect19 <jeffamenya388@gmail.com>
2025-01-23 15:22:49 +0300

    Working onboading state

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
-import { Image, View } from 'react-native';
+import { Image, View, Text } from 'react-native';
 import React from 'react'
 import Onboarding from 'react-native-onboarding-swiper';
 import { Redirect } from 'expo-router';
+import { SvgUri } from 'react-native-svg';
 
 import images from '../constants/images';
+import svgs from '../constants/svgs';
 
 const NavigateHome = () => {
   return <Redirect href={'/home'} />
@@ -17,30 +19,50 @@ const Welcome = () => (
       {
         backgroundColor: '#151320',
         image: 
-          <View className='w-full'>
-            <Image resizeMode='cover' source={images.rafiki3} className=' bg-[#151320]' />
+          <View className=' fixed w-full h-full bg-white p-0 m-0 top-0 left-0 bottom-0 right-0'>
+            <svgs.onboard_1 className='bg-slate-50'/>
+            <Text className='color-emerald-600'>Placeholder text</Text>
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
       },
       {
-        backgroundColor: '#13201c',
+        backgroundColor: '#151320',
         image: 
-          <View className='w-full' >
-            <Image source={images.pana} className=' bg-[#13201c]' />
+          <View className='w-full h-full'>
+            <svgs.onboard_2 />
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
       },
       {
-        backgroundColor: '#201313',
+        backgroundColor: '#151320',
         image: 
-          <View className='w-full' >
-            <Image source={images.rafiki3} className=' bg-[#201313]' />
+          <View className='w-full h-full'>
+            <svgs.onboard_3 />
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
       },
+      {
+        backgroundColor: '#151320',
+        image: 
+          <View className='w-full h-full'>
+            <svgs.onboard_4 />
+          </View>,
+        title: 'Onboarding',
+        subtitle: 'Done with React Native Onboarding Swiper',
+      },
+      {
+        backgroundColor: '#151320',
+        image: 
+          <View className='w-full h-full'>
+            <svgs.onboard_5 />
+          </View>,
+        title: 'Onboarding',
+        subtitle: 'Done with React Native Onboarding Swiper',
+      },
+      
     ]}
   />
 );
--- constants/images.js
+++ constants/images.js
@@ -1,13 +1,5 @@
 import favicon from '../assets/images/favicon.png';
-import rafiki from '../assets/images/sunny-day-rafiki.svg';
-import rafiki3 from '../assets/images/rafiki3.png';
-import kiduchu from '../assets/images/weather-kiduchu.svg';
-import pana from '../assets/images/weather-pana2.png';
 
 export default {
   favicon,
-  rafiki,
-  kiduchu,
-  pana,
-  rafiki3
 };
