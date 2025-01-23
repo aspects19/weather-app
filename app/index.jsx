456b800e83416c2063c2d4a921c7e40b6b0510d1
aspect19 <jeffamenya388@gmail.com>
2025-01-23 22:23:48 +0300

    Replace new svg onboard designs

--- app/index.jsx
+++ app/index.jsx
@@ -1,63 +1,65 @@
-import { Image, View, Text } from 'react-native';
-import React from 'react'
+import { Image, View, Text, useWindowDimensions } from 'react-native';
+import React, {useState, useEffect} from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
 import { Redirect } from 'expo-router';
-import { SvgUri } from 'react-native-svg';
 
 import images from '../constants/images';
 import svgs from '../constants/svgs';
+
+
 
 const NavigateHome = () => {
   return <Redirect href={'/home'} />
   
 };
 
-const Welcome = () => (
+const Welcome = () => {
+  const {width, height} = useWindowDimensions();
+  return (
   <Onboarding
-    onDone={() => NavigateHome()}
+    onDone={() => {}}
     pages={[
       {
-        backgroundColor: '#151320',
+        backgroundColor: 'black',
         image: 
           <View className=' fixed w-full h-full bg-white p-0 m-0 top-0 left-0 bottom-0 right-0'>
-            <svgs.onboard_1 className='bg-slate-50'/>
-            <Text className='color-emerald-600'>Placeholder text</Text>
+            <svgs.onboard_2  width={width} height={height} />
+          </View>,
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: 
+          <View className='w-full h-full'>
+            <svgs.onboard_1 width={width} height={height} />
+          </View>,
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: 
+          <View className='w-full h-full'>
+            <svgs.onboard_3 width={width} height={height} />
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
       },
       {
-        backgroundColor: '#151320',
+        backgroundColor: 'black',
         image: 
           <View className='w-full h-full'>
-            <svgs.onboard_2 />
+            <svgs.onboard_4 width={width} height={height} />
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
       },
       {
-        backgroundColor: '#151320',
+        backgroundColor: 'black',
         image: 
           <View className='w-full h-full'>
-            <svgs.onboard_3 />
-          </View>,
-        title: 'Onboarding',
-        subtitle: 'Done with React Native Onboarding Swiper',
-      },
-      {
-        backgroundColor: '#151320',
-        image: 
-          <View className='w-full h-full'>
-            <svgs.onboard_4 />
-          </View>,
-        title: 'Onboarding',
-        subtitle: 'Done with React Native Onboarding Swiper',
-      },
-      {
-        backgroundColor: '#151320',
-        image: 
-          <View className='w-full h-full'>
-            <svgs.onboard_5 />
+            <svgs.onboard_5 width={width} height={height} />
           </View>,
         title: 'Onboarding',
         subtitle: 'Done with React Native Onboarding Swiper',
@@ -65,6 +67,7 @@ const Welcome = () => (
       
     ]}
   />
-);
+  )
+};
 
 export default Welcome;
