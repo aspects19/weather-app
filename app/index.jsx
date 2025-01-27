03279b596f6f49b1c7a4342811494470316bbbed
aspect19 <jeffamenya388@gmail.com>
2025-01-28 00:25:28 +0300

    Fix visible headers un home and setting

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { Image, View, Text, useWindowDimensions, Alert, StyleSheet } from 'react
 import React, {useState, useEffect} from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
 import { Redirect, router } from 'expo-router';
+import { getItemAsync, setItemAsync } from '../components/asyncStorageReadWrite';
 
 import images from '../constants/images';
 import svgs from '../constants/svgs';
@@ -13,125 +14,128 @@ const Welcome = () => {
 
   const [onboardDone, setOnboardDone] = useState(false);
 
-  useEffect(() => {
-    const checkOnboardStatus = async () => {
-      const onboardStatus = await getItemAsync('onboardStatus');
-      if (onboardStatus !== null) {
-        setOnboardDone(onboardStatus);
-      }
-    };
-    checkOnboardStatus();
-  }, []);
+  // useEffect(() => {
+  //   const checkOnboardStatus = async () => {
+  //     const onboardStatus = await getItemAsync('onboardStatus');
+  //     if (onboardStatus !== null) {
+  //       setOnboardDone(onboardStatus);
+  //     }
+  //   };
+  //   checkOnboardStatus();
+  // }, []);
 
-  // if (onboardDone) {
-  //   return(
-  //   <Redirect href={'/home'} />
-  //   )
-  // };
-  
-  // return (
-  // <Onboarding
-  //   onDone={() => {setOnboardDone(true)}}
-  //   onSkip={() => {}}
-
-  //   pages={[
-  //     {
-  //       backgroundColor: 'black',
-  //       image:( 
-  //         <View style={styles.onboardImageContainer}>
-  //           <svgs.onboard_1 width={width} height={height} />
-  //           <View style={styles.onboardTextWrapper}>
-  //             <Text style={styles.onboardTitle} className='text-4xl'>
-  //              Stay Ready
-  //             </Text>
-  //             <Text style={styles.onboardSubtitle}>
-  //               Discover joy in every forecast, rain or shine.
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       ),
-  //       title: '',
-  //       subtitle: '',
-  //     },
-  //     {
-  //       backgroundColor: 'black',
-  //       image: ( 
-  //         <View style={styles.onboardImageContainer}>
-  //           <svgs.onboard_2 width={width} height={height} />
-  //           <View style={styles.onboardTextWrapper}>
-  //             <Text style={styles.onboardTitle}>
-  //               Plan Ahead
-  //             </Text>
-  //             <Text style={styles.onboardSubtitle}>
-  //               Get accurate weather forecasts to start your day right
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       ),
-  //       title: '',
-  //       subtitle: '',
-  //     },
-  //     {
-  //       backgroundColor: 'black',
-  //       image: ( 
-  //         <View style={styles.onboardImageContainer}>
-  //           <svgs.onboard_3 width={width} height={height} />
-  //           <View style={styles.onboardTextWrapper}>
-  //             <Text style={styles.onboardTitle}>
-  //               Stay Ready
-  //             </Text>
-  //             <Text style={styles.onboardSubtitle}>
-  //               Be prepared for any weather with real-time updates.
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       ),
-  //       title: '',
-  //       subtitle: '',
-  //     },
-  //     {
-  //       backgroundColor: 'black',
-  //       image: ( 
-  //         <View style={styles.onboardImageContainer}>
-  //           <svgs.onboard_4 width={width} height={height} />
-  //           <View style={styles.onboardTextWrapper}>
-  //             <Text style={styles.onboardTitle}>
-  //               Know Why 
-  //             </Text>
-  //             <Text style={styles.onboardSubtitle}>
-  //               Understand the weather with detailed explanations.
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       ),
-  //       title: '',
-  //       subtitle: '',
-  //     },
-  //     {
-  //       backgroundColor: 'black',
-  //       image: ( 
-  //         <View style={styles.onboardImageContainer}>
-  //           <svgs.onboard_5 width={width} height={height} />
-  //           <View style={styles.onboardTextWrapper}>
-  //             <Text style={styles.onboardTitle}>
-  //               Enjoy Every Moment
-  //             </Text>
-  //             <Text style={styles.onboardSubtitle}>
-  //             Discover joy in every forecast, rain or shine.
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       ),
-  //       title: '',
-  //       subtitle: '',
-  //     },
-      
-  //   ]}
-  // />
-  // )
-  return(
+  if (onboardDone) {
+    return(
     <Redirect href={'/home'} />
     )
+  };
+  
+  return (
+  <Onboarding
+    onDone={() => {
+      setOnboardDone(true);
+      setItemAsync('onboardStatus', true);
+    }}
+    onSkip={() => {
+      setOnboardDone(true);
+      setItemAsync('onboardStatus', true);
+    }}
+
+    pages={[
+      {
+        backgroundColor: 'black',
+        image:( 
+          <View style={styles.onboardImageContainer}>
+            <svgs.onboard_1 width={width} height={height} />
+            <View style={styles.onboardTextWrapper}>
+              <Text style={styles.onboardTitle} className='text-4xl'>
+               Stay Ready
+              </Text>
+              <Text style={styles.onboardSubtitle}>
+                Discover joy in every forecast, rain or shine.
+              </Text>
+            </View>
+          </View>
+        ),
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: ( 
+          <View style={styles.onboardImageContainer}>
+            <svgs.onboard_2 width={width} height={height} />
+            <View style={styles.onboardTextWrapper}>
+              <Text style={styles.onboardTitle}>
+                Plan Ahead
+              </Text>
+              <Text style={styles.onboardSubtitle}>
+                Get accurate weather forecasts to start your day right
+              </Text>
+            </View>
+          </View>
+        ),
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: ( 
+          <View style={styles.onboardImageContainer}>
+            <svgs.onboard_3 width={width} height={height} />
+            <View style={styles.onboardTextWrapper}>
+              <Text style={styles.onboardTitle}>
+                Stay Ready
+              </Text>
+              <Text style={styles.onboardSubtitle}>
+                Be prepared for any weather with real-time updates.
+              </Text>
+            </View>
+          </View>
+        ),
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: ( 
+          <View style={styles.onboardImageContainer}>
+            <svgs.onboard_4 width={width} height={height} />
+            <View style={styles.onboardTextWrapper}>
+              <Text style={styles.onboardTitle}>
+                Know Why 
+              </Text>
+              <Text style={styles.onboardSubtitle}>
+                Understand the weather with detailed explanations.
+              </Text>
+            </View>
+          </View>
+        ),
+        title: '',
+        subtitle: '',
+      },
+      {
+        backgroundColor: 'black',
+        image: ( 
+          <View style={styles.onboardImageContainer}>
+            <svgs.onboard_5 width={width} height={height} />
+            <View style={styles.onboardTextWrapper}>
+              <Text style={styles.onboardTitle}>
+                Enjoy Every Moment
+              </Text>
+              <Text style={styles.onboardSubtitle}>
+              Discover joy in every forecast, rain or shine.
+              </Text>
+            </View>
+          </View>
+        ),
+        title: '',
+        subtitle: '',
+      },
+      
+    ]}
+  />
+  )
 };
 
 export default Welcome;
